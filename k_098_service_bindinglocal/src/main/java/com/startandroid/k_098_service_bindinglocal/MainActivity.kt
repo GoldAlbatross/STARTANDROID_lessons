6fcee1b30d63468b684cfd992365805092202b47
unknown <snovaodin@gmail.com>
2023-06-01 19:56:42 +0300

    test

--- k_098_service_bindinglocal/src/main/java/com/startandroid/k_098_service_bindinglocal/MainActivity.kt
+++ k_098_service_bindinglocal/src/main/java/com/startandroid/k_098_service_bindinglocal/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.startandroid.k_098_service_bindinglocal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.startandroid.k_098_service_bindinglocal.MyService.MyBinder
+import com.startandroid.k_098_service_bindinglocal.databinding.ActivityMainBinding
 
+const val LOG = "qqq"
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private val binding by lazy (LazyThreadSafetyMode.NONE){
+        ActivityMainBinding.inflate(layoutInflater) }
+
+  private var bound = false
+  private lateinit var serviceConnection: ServiceConnection
+  private lateinit var intent: Intent
+  private lateinit var service: MyService
+  var interval: Long = 0
+
+  /** Called when the activity is first created.  */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(binding.root)
+
+    intent = Intent(this, MyService::class.java)
+    serviceConnection = object : ServiceConnection {
+      override fun onServiceConnected(name: ComponentName?, binder: IBinder) {
+        Log.d(LOG, "MainActivity onServiceConnected")
+        service = (binder as MyBinder).service
+        bound = true
+      }
+      override fun onServiceDisconnected(name: ComponentName?) {
+        Log.d(LOG, "MainActivity onServiceDisconnected")
+        bound = false
+      }
     }
+    setListeners()
+  }
+
+
+  override fun onStart() {
+    super.onStart()
+    bindService(intent, serviceConnection, 0)
+  }
+
+  override fun onStop() {
+    super.onStop()
+    if (!bound) return
+    unbindService(serviceConnection)
+    bound = false
+  }
+
+  private fun setListeners() {
+    binding.apply {
+      btnStart.setOnClickListener { startService(intent) }
+      btnUp.setOnClickListener { onClickUp() }
+      btnDown.setOnClickListener { onClickDown() }
+    }
+  }
+
+  private fun onClickUp() {
+    if (!bound) return
+    interval = service.upInterval(500)
+    binding.interval.text = "interval = $interval"
+  }
+
+  private fun onClickDown() {
+    if (!bound) return
+    interval = service.downInterval(500)
+    binding.interval.text = "interval = $interval"
+  }
 }
---
+++ k_098_service_bindinglocal/src/main/java/com/startandroid/k_098_service_bindinglocal/MyService.kt
@@ -0,0 +1,58 @@
+package com.startandroid.k_098_service_bindinglocal
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import java.util.Timer
+import java.util.TimerTask
+
+class MyService: Service() {
+
+
+    private var binder = MyBinder()
+    private lateinit var timer: Timer
+    private lateinit var timerTask: TimerTask
+    private var interval: Long = 1000
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(LOG, "Service onCreate")
+        timer = Timer()
+        schedule()
+    }
+
+    override fun onBind(arg0: Intent?): IBinder {
+        Log.d(LOG, "Service onBind")
+        return binder
+    }
+
+    private fun schedule() {
+        timerTask.cancel()
+        if (interval > 0) {
+            timerTask = object : TimerTask() {
+                override fun run() { Log.d(LOG, "run") }
+            }
+            timer.schedule(timerTask, 1000, interval)
+        }
+    }
+
+    fun upInterval(gap: Long): Long {
+        interval += gap
+        schedule()
+        return interval
+    }
+
+    fun downInterval(gap: Long): Long {
+        interval -= gap
+        if (interval < 0) interval = 0
+        schedule()
+        return interval
+    }
+
+    inner class MyBinder : Binder() {
+        val service: MyService
+            get() = this@MyService
+    }
+}
